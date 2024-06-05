61ac9a613d61f1f77e63d8a5e6b7c871e591a94d
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-06-05 10:21:51 +0530

    Add middleware to handle error

--- api/index.js
+++ api/index.js
@@ -9,11 +9,11 @@ const app = express();
 
 app.use(express.json());
 mongoose.connect(process.env.MONGO)
-.then(() => {
-    console.log("MongoDb is connected")
-}).catch((err) => {
-    console.log("Connection Filed", err);
-});
+    .then(() => {
+        console.log("MongoDb is connected")
+    }).catch((err) => {
+        console.log("Connection Filed", err);
+    });
 
 app.listen(3000, () => {
     console.log("Server is running on Port 3000");
@@ -21,5 +21,16 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server Error";
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
 
 //npm run dev
