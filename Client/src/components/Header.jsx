8e7d7275132887385b1a77e60d9428133ab382c4
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-06-07 12:54:29 +0530

    Update Header With user Data

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,14 +1,18 @@
-import { Navbar, TextInput, Button } from 'flowbite-react';
+import { Avatar, Dropdown, Navbar, TextInput } from 'flowbite-react';
+import { Button } from 'flowbite-react';
 import { Link } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
 import { useLocation } from 'react-router-dom';
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
     const path = useLocation().pathname;
+    const {currentUser} = useSelector(state => state.user);
+
     return (
-        <Navbar className='border-b-2 h-16 '>
+        <Navbar className='border-b-2 bg-aliceblue text-black h-full w-full p-3'>
             <Link
                 to='/'
                 className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
@@ -18,17 +22,14 @@ export default function Header() {
                 </span>
                 Blog
             </Link>
-            <form >
+            <form className="hidden lg:block">
                 <TextInput
                     type='text'
                     placeholder='Search...'
                     rightIcon={AiOutlineSearch}
-                    className='hidden lg:inline'
+                    className="bg-white text-black"
                 />
             </form>
-            <Button className='w-12 h-10 lg:hidden' color='gray' pill>
-                <AiOutlineSearch />
-            </Button>
             <div className='flex gap-2 md:order-2'>
                 <Button className='w-12 h-10 lg:hidden' color='gray' pill>
                     <AiOutlineSearch />
@@ -36,12 +37,37 @@ export default function Header() {
                 <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
                     <FaMoon />
                 </Button>
-                <Link to='/sign-in'>
-                    <Button className='bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white font-bold py-2 px-4 rounded outline'>
-                        Sign In
-                    </Button>
-                </Link>
-
+                {currentUser ?
+                    (
+                        <Dropdown
+                            arrowIcon={false}
+                            inline
+                            label={
+                                <Avatar
+                                    alt='user'
+                                    img={currentUser.profilePicture}
+                                    rounded
+                                />
+                            }
+                        >
+                            <Dropdown.Header>
+                                <span className='block text-sm'>@{currentUser.username}</span>
+                                <span className='block text-sm font-medium truncate'>@{currentUser.email}</span>
+                            </Dropdown.Header>
+                            <Link to='/dashboard?tab=profile'>
+                            <Dropdown.Item>Profile</Dropdown.Item>
+                            </Link>
+                            <Dropdown.Divider />
+                            <Dropdown.Item>Sign Out</Dropdown.Item>
+                        </Dropdown>
+                    ) : (
+                        <Link to='/sign-in'>
+                            <Button className='bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white font-bold py-2 px-4 rounded outline'>
+                                Sign In
+                            </Button>
+                        </Link>
+                    )
+                }
                 <Navbar.Toggle />
             </div>
             <Navbar.Collapse >
