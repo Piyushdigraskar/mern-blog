553eafbcc2d72bbb143e3557e08fca72c008e58f
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-06-19 17:36:24 +0530

    Create getusers api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -61,9 +61,51 @@ export const updateUser = async(req, res, next)=>{
     }
 
 
-    export const signout = (req, res)=>{
+    export const signout = (req, res, next)=>{
         try {
             res.clearCookie('access-token').status(200).json('User has been successfully signed out');
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    export const getusers = async(req, res, next)=>{
+        if(!req.user.isAdmin){
+            return next(errorHandler('403', 'You are not allowed to see Users'));
+        }
+        try {
+            const startIndex = parseInt(req.query.startIndex) || 0;
+            const limit = parseInt(req.query.limit) || 9;
+            const sortDirection = req.query.sort === 'asc' ? 1 : -1;
+
+            const users = await User.find()
+            .sort({createdAt : sortDirection})
+            .skip(startIndex)
+            .limit(limit);
+
+            const usersWithoutPassword = users.map((user)=>{
+                const {password, ...rest} = user._doc;
+                return rest;
+            })
+
+            const totalUsers = await User.countDocuments();
+
+            const now = new Date();
+            const oneMonthAgo = new Date(
+                now.getFullYear(),
+                now.getMonth() - 1,
+                now.getDate()
+            );
+
+            const lastMonthUsers = await User.countDocuments(
+                {createdAt: {$gte:oneMonthAgo}}
+            )
+
+            res.status(200).json({
+                users: usersWithoutPassword,
+                totalUsers,
+                lastMonthUsers
+            })
         } catch (error) {
             next(error);
         }
