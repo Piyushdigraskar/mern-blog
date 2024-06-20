129b8d8fa2b87049cb6ee453f4945b7e8f82dea8
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-06-20 16:55:17 +0530

    Complete Post Page Functionality

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -13,6 +13,7 @@ import PrivatRoute from './components/PrivatRoute';
 import OnlyAdminPrivatRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
+import PostPage from './pages/PostPage';
 import 'flowbite/dist/flowbite.css';
 
 export default function App() {
@@ -32,6 +33,7 @@ export default function App() {
           <Route path="/update-post/:postId" element={<UpdatePost />} />
         </Route>
         <Route path="/projects" element={<Projects />} />
+        <Route path="/post/:postSlug" element={<PostPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
