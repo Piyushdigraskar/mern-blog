1be62a2bab21fc3556241f2043a6fcf7c1a0a573
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-06-26 12:35:44 +0530

    Add Comment Section to Admin Dashboard

--- Client/src/components/DashSidebar.jsx
+++ Client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Sidebar } from 'flowbite-react';
-import { HiArrowSmRight, HiUser, HiDocumentText, HiOutlineUserGroup } from 'react-icons/hi';
+import { HiArrowSmRight, HiUser, HiDocumentText, HiOutlineUserGroup, HiAnnotation } from 'react-icons/hi';
 import { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -61,6 +61,13 @@ export default function DashSidebar() {
             <Link to='/dashboard?tab=users'>
               <Sidebar.Item active={tab === 'users'} icon={HiOutlineUserGroup} as='div'>
                 Users
+              </Sidebar.Item>
+            </Link>
+          )}
+          {currentUser.isAdmin && (
+            <Link to='/dashboard?tab=comments'>
+              <Sidebar.Item active={tab === 'comments'} icon={HiAnnotation} as='div'>
+                Comments
               </Sidebar.Item>
             </Link>
           )}
--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createComment, getPostComments, likeComment, editComment, deleteComment } from '../controllers/comment.controller.js';
+import { createComment, getPostComments, likeComment, editComment, deleteComment, getComments } from '../controllers/comment.controller.js';
 import { verifyToken } from '../utils/verifyuser.js';
 
 const router = express.Router();
@@ -9,6 +9,7 @@ router.get('/getpostcomments/:postId', getPostComments);
 router.put('/likecomment/:commentId', verifyToken, likeComment);
 router.put('/editcomment/:commentId', verifyToken, editComment);
 router.delete('/deletecomment/:commentId', verifyToken, deleteComment);
+router.get('/getcomments',verifyToken, getComments);
 
 
 export default router;
